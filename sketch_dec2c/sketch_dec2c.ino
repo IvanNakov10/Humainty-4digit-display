539d0d6668d1382218cb85b7843e0d42af9f154b
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2022-12-02 19:38:29 +0200

    Humanity Sensor v0.02b

--- sketch_dec2c/sketch_dec2c.ino
+++ sketch_dec2c/sketch_dec2c.ino
@@ -6,17 +6,20 @@
 int CLK = 2;
 int DIA = 3;
 
-#define DHTTYPE DHT11;
-#define DHTPIN A0;
+#define DHTTYPE DHT11
+#define DHTPIN A0
 
 TM1637 tm1637(CLK, DIA);
 TM1637 tm(2, 3);
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTTYPE, DHTPIN);
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial.println("Test test");
+  tm1637.init();
+  tm1637.setBrightness(7);
 }
 
 void loop() {
