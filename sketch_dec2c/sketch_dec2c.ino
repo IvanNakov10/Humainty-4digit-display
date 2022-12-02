2dcbe848b64ead98111436211ca47172f9a98e19
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2022-12-02 19:51:23 +0200

    Humanity Sensor v0.1b

--- sketch_dec2c/sketch_dec2c.ino
+++ sketch_dec2c/sketch_dec2c.ino
@@ -2,27 +2,37 @@
 
 #include <DHT.h>
 #include <DHT_U.h>
-
 int CLK = 2;
-int DIA = 3;
+int DIO = 3;
 
-#define DHTTYPE DHT11
+TM1637 tm1637(2, 3);
+TM1637 tm(CLK, DIO);
+
 #define DHTPIN A0
+#define DHTTYPE DHT11 
 
-TM1637 tm1637(CLK, DIA);
-TM1637 tm(2, 3);
-
-DHT dht(DHTTYPE, DHTPIN);
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  Serial.println("Test test");
   tm1637.init();
-  tm1637.setBrightness(7);
+  tm1637.setBrightness(6);
+  dht.begin();
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-}
+  delay(2000);
+  float hum = dht.readHumidity();
+ 
+  if (isnan(hum )) {
+  Serial.println("Error: Failed to read hum");
+  return;
+  }
+  Serial.print(hum);
+  Serial.println();
+ 
+  tm1637.display(hum);
+  }
